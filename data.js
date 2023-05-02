ff876ac567780d243303ee4e9e0a8aab0847b6bd
ShivaSharanNavada <shivasharannavada.int@niveussolutions.com>
2023-05-02 10:58:16 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+  { id: 1, name: "Ellery" },
+  { id: 2, name: "Nealon" },
+  { id: 3, name: "Cunnah" },
+  { id: 4, name: "Ralfe" },
+  { id: 5, name: "Bloor" },
+  { id: 6, name: "Berth" },
+  { id: 7, name: "Sissens" },
+  { id: 8, name: "Roulston" },
+  { id: 9, name: "Maiklem" },
+  { id: 10, name: "Chasteney" }
+];
+
+module.exports = data;
---
+++ post.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const data = require("./data");
+const app = express();
+
+//creates a express server in port number 3000 and it prints in console
+app.listen(3000, () => {
+  console.log(`Server running on port 3000`);
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "api is working" });
+});
+
+//this will fetch the data from json and displays
+app.get("/api/data", (req, res) => {
+  res.json(data);
+});
+
+//post request
+//i have used thunder client vs-code extention to post a request and data is sent in body -> json and data will be rendering with existing...
+app.use(express.json());
+app.post("/api/data", (req, res) => {
+  const user = {
+    id: data.length + 1,
+    name: req.body.name,
+  };
+  data.push(user);
+  res.json(user);
+});
