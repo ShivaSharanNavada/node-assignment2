34bda3eb8699100d093cbb3eb104da7f57904e7d
ShivaSharanNavada <129269926+ShivaSharanNavada@users.noreply.github.com>
2023-05-02 11:00:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ const data = require("./data");
 
 const app = express();
 
-//creates a express server in port number 3000 and it prints in console
+//creates a express server in port number 3000 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-//Additional.. if path : `localhost:3000`  then it will redirect to given path
-app.get("/", (req, res) => {
-  res.statusCode = 302;
-  res.setHeader("Location", "http://localhost:3000/api/001?name=safwan&age=23");
-  res.end();
-});
+
 
 //this will take the request query from the url/path given, /api/001?name=hello&age=23
 app.get("/api/:id", (req, res) => {
